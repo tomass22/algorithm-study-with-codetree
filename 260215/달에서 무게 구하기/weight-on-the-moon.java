3284536c3fe5138f0e9f38726b4ce6918c0d9700
tomass22 <99718084+tomass22@users.noreply.github.com>
2026-02-15 23:57:30 +0900

    Title: 달에서 무게 구하기, Time: 98ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260215/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int pendulum = 13;
+        double g = 0.165;
+
+        System.out.printf("%d * %.6f = %.6f", pendulum, g, pendulum * g);
+    }
+}
