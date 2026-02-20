edc3605189837d2c275e7ea983bec9e32cc36206
tomass22 <99718084+tomass22@users.noreply.github.com>
2026-02-21 00:45:05 +0900

    Title: 문자 출력하기, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260221/문자 출력하기/print-text.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        String n = sc.next();
+
+        for (int i = 0; i < 8; i++) {
+            System.out.print(n);
+        }
+    }
+}
