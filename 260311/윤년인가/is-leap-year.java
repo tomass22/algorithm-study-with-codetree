4fa7177bea1f725fe08701291ea3e7245c14b54f
tomass22 <99718084+tomass22@users.noreply.github.com>
2026-03-11 18:54:11 +0900

    Title: 윤년인가, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

--- 260311/윤년인가/is-leap-year.java
+++ 260311/윤년인가/is-leap-year.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // "4년마다 오지만, 너무 자주 오면 오차가 생기니 100년 단위로 한 번씩 건너뛰고, 다시 400년 단위로는 포함시킨다"
+
         int y = sc.nextInt();
 
         if (y % 4 != 0 || (y % 100 == 0 && y % 400 != 0)) {
